c81e40009de4f5423e617ec5e287f1b7eaca8112
Aaron Lifton <aaronlifton@gmail.com>
2017-12-06 16:08:11 -0800

    remove dependency on spree_frontend

--- app/overrides/views_decorator.rb
+++ app/overrides/views_decorator.rb
@@ -13,20 +13,6 @@ Deface::Override.new(
   :disabled => false)
 
 Deface::Override.new(
-  :virtual_path => "spree/checkout/_payment",
-  :name => "store_credits_checkout_payment_step",
-  :insert_after => "[data-hook='checkout_payment_step']",
-  :partial => "spree/checkout/store_credits",
-  :disabled => false)
-
-Deface::Override.new(
-  :virtual_path => "spree/users/show",
-  :name => "store_credits_account_my_orders",
-  :insert_after => "[data-hook='account_my_orders']",
-  :partial => "spree/users/store_credits",
-  :disabled => false)
-
-Deface::Override.new(
   :virtual_path => "spree/admin/general_settings/edit",
   :name => "admin_general_settings_edit_for_sc",
   :insert_before => "[data-hook='buttons']",
